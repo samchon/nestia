fea2f27e5eff6f6076c19af5b4a9567f4230f755
Jeongho Nam <samchon.github@gmail.com>
2022-10-16 16:05:23 +0900

    Close #165 - let `npx nestia start` to clone repo

--- src/executable/internal/NestiaStarter.ts
+++ src/executable/internal/NestiaStarter.ts
@@ -1,8 +1,6 @@
 import cp from "child_process";
 import fs from "fs";
 
-const PATH = __dirname + "/../../../template";
-
 export namespace NestiaStarter {
     export async function start(dest: string | undefined): Promise<void> {
         // VALIDATION
@@ -15,37 +13,26 @@ export namespace NestiaStarter {
         console.log("-----------------------------------------");
 
         // COPY PROJECTS
-        console.log("Copying files...");
-        await copy(PATH, dest);
-        await copy(PATH + "/bundle/gitignore", dest + "/.gitignore");
-        await copy(PATH + "/bundle/npmignore", dest + "/.npmignore");
+        execute(`git clone https://github.com/samchon/nestia-template ${dest}`);
+        console.log(`cd "${dest}"`);
         process.chdir(dest);
 
         // INSTALL DEPENDENCIES
-        console.log("Installing dependencies...");
-        cp.execSync("npm install", { stdio: "inherit" });
+        execute("npm install");
 
         // BUILD TYPESCRIPT
-        console.log("Building project...");
-        cp.execSync("npm run build", { stdio: "inherit" });
+        execute("npm run build");
 
         // DO TEST
-        console.log("Running test program...");
-        cp.execSync("npm run test", { stdio: "inherit" });
+        execute("npm run test");
+
+        // REMOVE .GIT DIRECTORY
+        cp.execSync("npx rimraf .git");
     }
 
-    async function copy(src: string, dest: string): Promise<void> {
-        const stats: fs.Stats = await fs.promises.lstat(src);
-        if (stats.isDirectory()) {
-            if (src === PATH + "/.git" || src === PATH + "/bundle") return;
-
-            await fs.promises.mkdir(dest);
-            for (const file of await fs.promises.readdir(src))
-                await copy(src + "/" + file, dest + "/" + file);
-        } else {
-            const content: string = await fs.promises.readFile(src, "utf8");
-            await fs.promises.writeFile(dest, content, "utf8");
-        }
+    function execute(command: string): void {
+        console.log(command);
+        cp.execSync(command, { stdio: "inherit" });
     }
 }
 
