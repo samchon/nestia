fdc75cb0e0e97ff915aa0028b93d7e6d87be8c1e
Jeongho Nam <samchon.github@gmail.com>
2022-12-17 14:56:57 +0900

    Close #185 - `nestia` became CLI tool

---
+++ src/NestiaSetupWizard.ts
@@ -0,0 +1,70 @@
+import cp from "child_process";
+import fs from "fs";
+
+export namespace NestiaSetupWizard {
+    export async function ttypescript(manager: string): Promise<void> {
+        await prepare(manager);
+        const { CoreSetupWizard } = await import(
+            "@nestia/core/lib/executable/internal/CoreSetupWizard"
+        );
+        CoreSetupWizard.ttypescript(manager);
+    }
+
+    export async function tsPatch(manager: string): Promise<void> {
+        await prepare(manager);
+        const { CoreSetupWizard } = await import(
+            "@nestia/core/lib/executable/internal/CoreSetupWizard"
+        );
+        CoreSetupWizard.tsPatch(manager);
+    }
+
+    export async function prepare(manager: string): Promise<any> {
+        if (fs.existsSync("package.json") === false)
+            halt(() => {})("make package.json file or move to it.");
+
+        const pack: any = JSON.parse(
+            await fs.promises.readFile("package.json", "utf8"),
+        );
+        add(manager)(pack)("@nestia/core", false);
+        add(manager)(pack)("@nestia/sdk", false);
+    }
+}
+
+const add =
+    (manager: string) =>
+    (pack: any) =>
+    (modulo: string, devOnly: boolean): void => {
+        const exists: boolean =
+            (devOnly === false
+                ? !!pack.dependencies && !!pack.dependencies[modulo]
+                : !!pack.devDependencies && !!pack.devDependencies[modulo]) &&
+            fs.existsSync("node_modules/" + modulo);
+        const middle: string =
+            manager === "yarn"
+                ? `add${devOnly ? " -D" : ""}`
+                : `install ${devOnly ? "--save-dev" : "--save"}`;
+        if (exists === false) execute(`${manager} ${middle} ${modulo}`);
+    };
+
+const remove =
+    (manager: string) =>
+    (modulo: string, devOnly: boolean): void => {
+        const middle: string =
+            manager === "yarn"
+                ? `remove${devOnly ? " -D" : ""}`
+                : `uninstall ${devOnly ? "--save-dev" : "--save"}`;
+        execute(`${manager} ${middle} ${modulo}`);
+    };
+
+const halt =
+    (closer: () => any) =>
+    (desc: string): never => {
+        closer();
+        console.error(desc);
+        process.exit(-1);
+    };
+
+function execute(command: string): void {
+    console.log(command);
+    cp.execSync(command, { stdio: "inherit" });
+}
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { CommandParser } from "./CommandParser";
+import { NestiaSetupWizard } from "./NestiaSetupWizard";
+import { NestiaStarter } from "./NestiaStarter";
+
+const USAGE = `Wrong command has been detected. Use like below:
+
+npx nestia [command] [options?]
+
+  1. npx nestia start <directory> ---manager (npm|pnpm|yarn)
+    - npx nestia start project
+    - npx nestia start project --manager pnpm
+  2. npx nestia setup \
+        --compiler (ttypescript|ts-patch) \
+        --manager (npm|pnpm|yarn)
+    - npx nestia setup
+    - npx nestia setup --compiler ttypescript
+    - npx nestia setup --compiler ts-patch
+    - npx nestia setup --manager pnpm
+  3. npx nestia dependencies --manager (npm|pnpm|yarn)
+    - npx nestia dependencies
+    - npx nestia dependencies --manager yarn
+  4. npx nestia init
+  5. npx nestia sdk <input> --out <output>
+    - npx nestia sdk # when "nestia.config.ts" be configured
+    - npx nestia sdk src/controllers --out src/api
+    - npx nestia sdk src/**/*.controller.ts --out src/api
+  6. npx nestia swagger <input> --out <output>
+    - npx nestia swagger # when "nestia.config.ts" be configured
+    - npx nestia swagger src/controllers --out src/api
+    - npx nestia swagger src/**/*.controller.ts --out src/api       
+`;
+
+function halt(desc: string): never {
+    console.error(desc);
+    process.exit(-1);
+}
+
+async function setup(): Promise<void> {
+    const options: Record<string, string> = CommandParser.parse(
+        process.argv.slice(3),
+    );
+    const manager: string = options.manager ?? "npm";
+    const compiler: string = options.compiler ?? "ttypescript";
+
+    if (
+        (compiler !== "ttypescript" && compiler !== "ts-patch") ||
+        (manager !== "npm" && manager !== "pnpm" && manager !== "yarn")
+    )
+        halt(USAGE);
+    else if (compiler === "ttypescript")
+        await NestiaSetupWizard.ttypescript(manager);
+    else await NestiaSetupWizard.tsPatch(manager);
+}
+
+async function main(): Promise<void> {
+    const type: string | undefined = process.argv[2];
+    const argv: string[] = process.argv.slice(3);
+
+    if (type === "start")
+        await NestiaStarter.start((msg) => halt(msg ?? USAGE))(argv);
+    else if (type === "setup") await setup();
+    else if (
+        type === "dependencies" ||
+        type === "init" ||
+        type === "sdk" ||
+        type === "swagger"
+    ) {
+        try {
+            await import("@nestia/sdk");
+        } catch {
+            halt(
+                `@nestia/sdk has not been installed. Run "npx nestia setup" before.`,
+            );
+        }
+        await import("@nestia/sdk/lib/executable/sdk");
+    } else halt(USAGE);
+}
+main().catch((exp) => {
+    console.log(exp.message);
+    process.exit(-1);
+});
