e788e4dab55929d14dab54b5ed08b69bf44a7038
Jeongho Nam <samchon.github@gmail.com>
2022-12-23 15:58:34 +0900

    Close #204 - turn of numeric options in request body

--- packages/core/src/options/INestiaTransformOptions.ts
+++ packages/core/src/options/INestiaTransformOptions.ts
@@ -1,6 +1,4 @@
-import { ITransformOptions } from "typia/lib/transformers/ITransformOptions";
-
-export interface INestiaTransformOptions extends ITransformOptions {
+export interface INestiaTransformOptions {
     validate?: "assert" | "is" | "validate";
     stringify?: "stringify" | "assert" | "is" | "validate" | null;
 }
--- packages/core/src/transform.ts
+++ packages/core/src/transform.ts
@@ -1,14 +1,14 @@
 import ts from "typescript";
-import { IProject } from "typia/lib/transformers/IProject";
 
 import { INestiaTransformOptions } from "./options/INestiaTransformOptions";
+import { INestiaTransformProject } from "./options/INestiaTransformProject";
 import { FileTransformer } from "./transformers/FileTransformer";
 
 export default function transform(
     program: ts.Program,
     options?: INestiaTransformOptions,
 ): ts.TransformerFactory<ts.SourceFile> {
-    const project: IProject = {
+    const project: INestiaTransformProject = {
         program,
         compilerOptions: program.getCompilerOptions(),
         checker: program.getTypeChecker(),
