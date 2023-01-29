c2322ebe1fef60675e04f41447e32fb038ee575f
Jeongho Nam <samchon.github@gmail.com>
2023-01-29 14:51:47 +0900

    Fix SwaggerGenerator bug, writing duplicated security values

--- packages/sdk/src/generates/SwaggerGenerator.ts
+++ packages/sdk/src/generates/SwaggerGenerator.ts
@@ -190,7 +190,7 @@ export namespace SwaggerGenerator {
         security: Required<INestiaConfig.ISwaggerConfig>["security"],
         swagger: ISwaggerDocument,
     ): void {
-        swagger.security ??= [];
+        swagger.security = [];
         swagger.components.securitySchemes = {};
 
         for (const [key, value] of Object.entries(security)) {
