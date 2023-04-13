f98b5a41b5b93a709b7f25863a66fabf977e7714
Jeongho Nam <samchon.github@gmail.com>
2023-04-14 04:25:55 +0900

    Complement #300: test in TypedRoute

--- packages/sdk/demo/status/src/controllers/StatusController.ts
+++ packages/sdk/demo/status/src/controllers/StatusController.ts
@@ -1,9 +1,10 @@
+import core from "@nestia/core";
 import * as nest from "@nestjs/common";
 
 @nest.Controller()
 export class StatusController {
     @nest.HttpCode(300)
-    @nest.Get("status")
+    @core.TypedRoute.Get("status")
     public status(): number {
         return 300;
     }
