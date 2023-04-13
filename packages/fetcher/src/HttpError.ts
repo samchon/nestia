2ea671029852fff22def8d508984b46759742fa9
Jeongho Nam <samchon.github@gmail.com>
2023-04-13 10:51:58 +0900

    Add `HttpError.toJSON()` method

--- packages/fetcher/src/HttpError.ts
+++ packages/fetcher/src/HttpError.ts
@@ -7,6 +7,11 @@
  */
 export class HttpError extends Error {
     /**
+     * @internal
+     */
+    private body_: any = NOT_YET;
+
+    /**
      * Initializer Constructor.
      *
      * @param method Method of the HTTP request.
@@ -27,4 +32,40 @@ export class HttpError extends Error {
         if (Object.setPrototypeOf) Object.setPrototypeOf(this, proto);
         else (this as any).__proto__ = proto;
     }
+
+    /**
+     * `HttpError` to JSON.
+     *
+     * When you call `JSON.stringify()` function on current `HttpError` instance,
+     * this `HttpError.toJSON()` method would be automatically called.
+     *
+     * Also, if response body from the remote HTTP server forms a JSON object,
+     * this `HttpError.toJSON()` method would be useful because it returns the
+     * parsed JSON object about the {@link message} property.
+     *
+     * @template T Expected type of the response body.
+     * @returns JSON object of the `HttpError`.
+     */
+    public toJSON<T>(): HttpError.IProps<T> {
+        if (this.body_ === NOT_YET) this.body_ = JSON.parse(this.message);
+        return {
+            method: this.method,
+            path: this.path,
+            status: this.status,
+            message: this.body_,
+        };
+    }
 }
+export namespace HttpError {
+    /**
+     * Returned type of {@link HttpError.toJSON} method.
+     */
+    export interface IProps<T> {
+        method: "GET" | "DELETE" | "POST" | "PUT" | "PATCH";
+        path: string;
+        status: number;
+        message: T;
+    }
+}
+
+const NOT_YET = {} as any;
