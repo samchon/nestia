bd90fd444f922cb49291908a97aed77364b3f3a3
Jeongho Nam <samchon.github@gmail.com>
2023-05-07 21:58:54 +0900

    Entire build script

---
+++ build/index.js
@@ -0,0 +1,31 @@
+const cp = require("child_process");
+const fs = require("fs");
+
+const PACKAGES = `${__dirname}/../packages`;
+const README = `${__dirname}/../README.md`;
+
+const execute = (command) => {
+    console.log(command);
+    cp.execSync(command, { stdio: "inherit" });
+};
+
+const main = fs.readdirSync(PACKAGES).forEach((lib) => {
+    const location = `${PACKAGES}/${lib}`;
+    if (fs.existsSync(`${location}/package.json`) === false)
+        return;
+
+    console.log("----------------------------------------");
+    console.log(`@nestia/${lib}`);
+    console.log("----------------------------------------");
+    process.chdir(location);
+
+    fs.copyFileSync(README, "README.md");
+
+    const test = !!JSON.parse(
+        fs.readFileSync("package.json", { encoding: "utf-8" })
+    ).scripts?.test;
+
+    execute("npm install");
+    execute("npm run build");
+    if (test) execute("npm run test");
+});
