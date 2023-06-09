f566365fa3e3f14efe7190aa5d422aca64592213
Jeongho Nam <samchon.github@gmail.com>
2023-06-10 01:57:59 +0900

    Complement #383 - publish to `4.3.0`

--- packages/cli/src/NestiaStarter.ts
+++ packages/cli/src/NestiaStarter.ts
@@ -7,17 +7,7 @@ export namespace NestiaStarter {
         async (argv: string[]): Promise<void> => {
             // VALIDATION
             const dest: string | undefined = argv[0];
-            const manager: string =
-                argv[1] === "--manager" && argv[2] ? argv[2] : "npm";
             if (dest === undefined) halter();
-            else if (
-                manager !== "npm" &&
-                manager !== "yarn" &&
-                manager !== "pnpm"
-            )
-                throw new Error(
-                    "Invalid package manager. Only npm, yarn, and pnpm are supported.",
-                );
             else if (fs.existsSync(dest) === true)
                 halter("The target directory already exists.");
 
@@ -33,7 +23,7 @@ export namespace NestiaStarter {
             process.chdir(dest);
 
             // INSTALL DEPENDENCIES
-            execute(manager === "yarn" ? "yarn" : `${manager} install`);
+            execute("npm install");
 
             // BUILD TYPESCRIPT
             execute("npm run build");
