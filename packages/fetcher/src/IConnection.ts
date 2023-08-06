a7d55212175a23fe55fd4624bb85eedf0c939013
Mateus Mamede Lage <mlage@bethel.jw.org>
2023-08-05 23:14:28 -0400

    Enhance #446 add signal to IConnection.IOptions

--- packages/fetcher/src/IConnection.ts
+++ packages/fetcher/src/IConnection.ts
@@ -134,6 +134,49 @@ export namespace IConnection {
             | "strict-origin"
             | "strict-origin-when-cross-origin"
             | "unsafe-url";
+
+        /**
+         * An AbortSignal to set request's signal.
+         */
+        signal?: AbortSignal | null;
+    }
+
+    /*
+     * `AbortSignal` is defined here to prevent a dependency on a particular
+     * implementation like the `abort-controller` package, and to avoid requiring
+     * the `dom` library in `tsconfig.json`.
+     */
+    export interface AbortSignal {
+        aborted: boolean;
+        reason: any;
+
+        addEventListener: (
+            type: "abort",
+            listener: (this: AbortSignal, event: any) => any,
+            options?:
+                | boolean
+                | {
+                      capture?: boolean;
+                      once?: boolean;
+                      passive?: boolean;
+                  },
+        ) => void;
+
+        removeEventListener: (
+            type: "abort",
+            listener: (this: AbortSignal, event: any) => any,
+            options?:
+                | boolean
+                | {
+                      capture?: boolean;
+                  },
+        ) => void;
+
+        dispatchEvent: (event: any) => boolean;
+
+        onabort: null | ((this: AbortSignal, event: any) => any);
+
+        throwIfAborted(): void;
     }
 
     /**
