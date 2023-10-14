cf27ca611bb2f5029d12fe814a8d809dc214d6eb
Jeongho Nam <samchon.github@gmail.com>
2023-10-15 02:04:45 +0900

    Close #656 -support multiple nestia.config.ts file.
    ```bash
    npx nestia sdk --config nestia.config.ts
    npx nestia swagger --config something.config.ts
    ```
    
    As `@nestia/sdk` has started supporting the propagation mode (#599), I think there can be some users who want to build SDK in both normal and propagation mode at the same time. To support such future requirement, I've decided to support multiple `nestia.config.ts` files by adding a new parameter `--config` in the CLI command.

--- packages/sdk/src/executable/internal/NestiaSdkCommand.ts
+++ packages/sdk/src/executable/internal/NestiaSdkCommand.ts
@@ -17,10 +17,12 @@ export namespace NestiaSdkCommand {
         task: (app: NestiaSdkApplication) => Promise<void>,
     ) => {
         // LOAD CONFIG INFO
-        const project: string = await NestiaConfigLoader.project();
+        const file: string = getConfigFile() ?? "nestia.config.ts";
+        const project: string = await NestiaConfigLoader.project(file);
         const compilerOptions: ts.CompilerOptions =
             await NestiaConfigLoader.compilerOptions(project);
         const config: INestiaConfig = await NestiaConfigLoader.config(
+            file,
             compilerOptions,
         );
 
@@ -30,5 +32,20 @@ export namespace NestiaSdkCommand {
             compilerOptions,
         );
         await task(app);
+    };
+
+    const getConfigFile = (): string | null => {
+        const argv: string[] = process.argv.slice(3);
+        if (argv.length < 1) return null;
+
+        const index: number = argv.findIndex((str) => str === "--config");
+        if (index === -1) return null;
+        else if (argv.length === 1)
+            throw new Error("Config file must be provided");
+
+        const file: string = argv[index + 1];
+        if (file.endsWith(".ts") === false)
+            throw new Error("Config file must be TypeScript file");
+        return file;
     };
 }
