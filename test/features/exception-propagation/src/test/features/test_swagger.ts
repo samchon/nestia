f0ae119c956560b60b35d264e182c908678dc00d
industriously <dev.rojiwon@gmail.com>
2023-11-08 14:50:28 +0900

    swagger test api 경로 변경

--- test/features/exception-propagation/src/test/features/test_swagger.ts
+++ test/features/exception-propagation/src/test/features/test_swagger.ts
@@ -3,26 +3,29 @@ import { TestValidator } from "@nestia/e2e";
 import { ErrorCode } from "../../structures/ErrorCode";
 
 export const test_swagger = async () => {
-    const test = async (
-        path:
-            | "/a-success-case"
-            | "/a-success-case/union"
-            | "/b-fail-case"
-            | "/b-fail-case/composite",
-        expected: string[],
-    ) => {
-        path;
+    try {
         const content = await import("../../../swagger.json");
-        const actual: string[] = (content["paths"][path] as any)["get"][
-            "responses"
-        ]["401"]["content"]["application/json"]["schema"]["enum"];
-        TestValidator.equals(path)(expected.sort())(actual.sort());
-    };
-    const required: ErrorCode.Permission.Required = "REQUIRED_PERMISSION";
-    const expired: ErrorCode.Permission.Expired = "EXPIRED_PERMISSION";
-    const invalid: ErrorCode.Permission.Invalid = "INVALID_PERMISSION";
-    test("/a-success-case", [invalid]);
-    test("/a-success-case/union", [expired, required]);
-    test("/b-fail-case", [expired, invalid]);
-    test("/b-fail-case/composite", [expired, invalid, required]);
+        const test = async (
+            path:
+                | "/success"
+                | "/success/{error_type}"
+                | "/fail/{error_type}"
+                | "/fail/composite/{error_type}",
+            expected: string[],
+        ) => {
+            const actual: string[] = (content["paths"] as any)[path]["get"][
+                "responses"
+            ]["401"]["content"]["application/json"]["schema"]["enum"];
+            TestValidator.equals(path)(expected.sort())(actual.sort());
+        };
+        const required: ErrorCode.Permission.Required = "REQUIRED_PERMISSION";
+        const expired: ErrorCode.Permission.Expired = "EXPIRED_PERMISSION";
+        const invalid: ErrorCode.Permission.Invalid = "INVALID_PERMISSION";
+        test("/success", [invalid]);
+        test("/success/{error_type}", [expired, required]);
+        test("/fail/{error_type}", [expired, invalid]);
+        test("/fail/composite/{error_type}", [expired, invalid, required]);
+    } catch (error) {
+        console.log((error as Error).stack ?? "");
+    }
 };
