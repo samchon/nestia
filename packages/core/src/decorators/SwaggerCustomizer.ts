58fb3906871f443f1ff9547feeb29dbc75fae18c
Jeongho Nam <samchon.github@gmail.com>
2024-02-23 22:17:48 +0900

    Documentate #810

--- packages/core/src/decorators/SwaggerCustomizer.ts
+++ packages/core/src/decorators/SwaggerCustomizer.ts
@@ -1,6 +1,20 @@
 import { ISwagger } from "../structures/ISwagger";
 import { ISwaggerRoute } from "../structures/ISwaggerRoute";
 
+/**
+ * Swagger customization decorator.
+ *
+ * `SwaggerCustomizer` is a method decorator function which can used for
+ * customizing the swagger data with `npx nestia swagger` command. Furthermore,
+ * it is possible to add plugin properties starting with `x-` characters.
+ *
+ * In other words, this decorator function does not affect to the runtime,
+ * but only for the swagger data customization.
+ *
+ * @param closure Callback function which can customize the swagger data
+ * @returns Method decorator
+ * @author Jeongho Nam - https://github.com/samchon
+ */
 export function SwaggerCustomizer(
   closure: (props: SwaggerCustomizer.IProps) => unknown,
 ): MethodDecorator {
@@ -19,15 +33,55 @@ export function SwaggerCustomizer(
   };
 }
 export namespace SwaggerCustomizer {
+  /**
+   * Properties for the `SwaggerCustomizer` decorator.
+   *
+   * `SwaggerCustomizer.IProps` is a type for the `closure` parameter of the
+   * `SwaggerCustomizer` decorator. It's a callback function which can customize
+   * the swagger data.
+   */
   export interface IProps {
+    /**
+     * Swagger data.
+     */
     swagger: ISwagger;
+
+    /**
+     * Method of the route.
+     */
     method: string;
+
+    /**
+     * Path of the route.
+     */
     path: string;
+
+    /**
+     * Route data.
+     */
     route: ISwaggerRoute;
+
+    /**
+     * Get neighbor route data.
+     *
+     * @param accessor Accessor for getting neighbor route data
+     * @returns Neighbor route data
+     */
     get(accessor: IAccessor): ISwaggerRoute | undefined;
   }
+
+  /**
+   * Accessor for getting neighbor route data.
+   */
   export interface IAccessor {
+    /**
+     * Path of the neighbor route.
+     */
     path: string;
+
+    /**
+     * Method of the neighbor route.
+     */
     method: string;
   }
 }
--- test/features/swagger-customizer/src/controllers/SwaggerController.ts
+++ test/features/swagger-customizer/src/controllers/SwaggerController.ts
@@ -2,17 +2,21 @@ import { SwaggerCustomizer, TypedParam, TypedRoute } from "@nestia/core";
 import { Controller } from "@nestjs/common";
 import { tags } from "typia";
 
-@Controller("swagger")
-export class SwaggerController {
-  @SwaggerCustomizer(({ swagger, route, get }) => {
-    swagger.openapi = "3.0.11";
-    route.description = "This is a custom description";
+@Controller("custom")
+export class CustomController {
+  @SwaggerCustomizer((props: SwaggerCustomizer.IProps) => {
+    props.swagger.openapi = "3.0.11";
+    props.route.description = "This is a custom description";
+    (props.route as any)["x-special-symbol"] = "Something Special";
 
-    const neighbor = get({
+    const neighbor = props.get({
       method: "get",
-      path: "swagger/:id/normal",
+      path: "custom/:id/normal",
     });
-    if (neighbor) neighbor.description = "That is the normal description";
+    if (neighbor) {
+      neighbor.description = "That is the normal description";
+      (neighbor as any)["x-special-symbol"] = "Something Normal";
+    }
   })
   @TypedRoute.Get(":key/customize")
   public customize(@TypedParam("key") key: number): string {
