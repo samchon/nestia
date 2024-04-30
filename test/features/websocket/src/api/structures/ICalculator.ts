5d148771e9aed7122d52339f80a18a58dcb31b0a
Jeongho Nam <samchon.github@gmail.com>
2024-04-30 17:15:41 +0900

    Finalize WebSocket features with documentation

--- test/features/websocket/src/api/structures/ICalculator.ts
+++ test/features/websocket/src/api/structures/ICalculator.ts
@@ -1,4 +1,10 @@
+import { IQuery } from "./IQuery";
+
 export interface ICalculator {
+  getId(): string;
+  getQuery(): IQuery;
+  getPrecision(): number;
+
   plus(x: number, y: number): number;
   minus(x: number, y: number): number;
   multiply(x: number, y: number): number;
