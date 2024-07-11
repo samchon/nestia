4106018236aac9b1395ae651ea35343fc947b6cc
Jeongho Nam <samchon.github@gmail.com>
2024-07-11 22:39:58 +0900

    Close #880: support multiple openapi versions generation.

---
+++ test/features/openapi_v2/src/test/features/test_openapi_v2.ts
@@ -0,0 +1,8 @@
+import { SwaggerV2 } from "@samchon/openapi";
+import typia from "typia";
+
+import swagger from "../../../swagger.json";
+
+export const test_openapi_v2 = (): void => {
+  typia.assert<SwaggerV2.IDocument>(swagger);
+}
