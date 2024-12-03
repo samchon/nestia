ac1ad3e701e1c16db5d96b5be00a7b1d48f1b5ba
Jeongho Nam <samchon.github@gmail.com>
2024-12-03 23:57:12 +0900

    Close samchon/typia#1366: discord server

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,39 +1,47 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
 
 const config: DocsThemeConfig = {
   logo: () => (
     <>
-      <img src="/favicon/android-chrome-192x192.png" 
-           width={32} 
-           height={32} />
-      <span style={{ 
-            fontWeight: "bold", 
-            fontSize: "1.2rem",
-            paddingLeft: 10,
-            paddingRight: 10, }}>
+      <img src="/favicon/android-chrome-192x192.png" width={32} height={32} />
+      <span
+        style={{
+          fontWeight: "bold",
+          fontSize: "1.2rem",
+          paddingLeft: 10,
+          paddingRight: 10,
+        }}
+      >
         Nestia
       </span>
       <span>NestJS Helper Libraries</span>
     </>
   ),
   project: {
-    link: 'https://github.com/samchon/nestia',
+    link: "https://github.com/samchon/nestia",
     // icon: <img
-    //   alt="Nestia Github repo stars" 
-    //   src="https://img.shields.io/github/stars/samchon/nestia?style=social" 
+    //   alt="Nestia Github repo stars"
+    //   src="https://img.shields.io/github/stars/samchon/nestia?style=social"
     // />
   },
-  docsRepositoryBase: 'https://github.com/samchon/nestia/blob/master/website',
+  chat: {
+    link: "https://discord.gg/E94XhzrUCZ",
+  },
+  docsRepositoryBase: "https://github.com/samchon/nestia/blob/master/website",
   footer: {
-    text: () => <span>
-      Made by{" "}
-      <a href="https://github.com/samchon" 
-         target="_blank" 
-         style={{ color: "blue" }}>
-        <u> Samchon </u>
-      </a>
-    </span>
+    text: () => (
+      <span>
+        Made by{" "}
+        <a
+          href="https://github.com/samchon"
+          target="_blank"
+          style={{ color: "blue" }}
+        >
+          <u> Samchon </u>
+        </a>
+      </span>
+    ),
   },
   useNextSeoProps() {
     return {
@@ -43,27 +51,27 @@ const config: DocsThemeConfig = {
         {
           rel: "apple-touch-icon",
           sizes: "180x180",
-          href: "/favicon/apple-touch-icon.png"
+          href: "/favicon/apple-touch-icon.png",
         },
         {
           rel: "manifest",
-          href: "/favicon/site.webmanifest"
+          href: "/favicon/site.webmanifest",
         },
-        ...[16, 32].map(size => ({
+        ...[16, 32].map((size) => ({
           rel: "icon",
           type: "image/png",
           sizes: `${size}x${size}`,
-          href: `/favicon/favicon-${size}x${size}.png`
+          href: `/favicon/favicon-${size}x${size}.png`,
         })),
       ],
       additionalMetaTags: [
         {
           property: "og:image",
-          content: "/og.jpg"
+          content: "/og.jpg",
         },
         {
           property: "og:type",
-          content: "object"
+          content: "object",
         },
         {
           property: "og:title",
@@ -71,39 +79,39 @@ const config: DocsThemeConfig = {
         },
         {
           property: "og:description",
-          content: "NestJS Helper Libraries"
+          content: "NestJS Helper Libraries",
         },
         {
           property: "og:site_name",
-          content: "Nestia Guide Documents"
+          content: "Nestia Guide Documents",
         },
         {
           property: "og:url",
-          content: "https://nestia.io"
+          content: "https://nestia.io",
         },
         {
           name: "twitter:card",
-          content: "summary"
+          content: "summary",
         },
         {
           name: "twitter:image",
-          content: "https://nestia.io/og.jpg"
+          content: "https://nestia.io/og.jpg",
         },
         {
           name: "twitter:title",
-          content: "Nestia Guide Documents"
+          content: "Nestia Guide Documents",
         },
         {
           name: "twitter:description",
-          content: "NestJS Helper Libraries"
+          content: "NestJS Helper Libraries",
         },
         {
           name: "twitter:site",
-          content: "@SamchonGithub"
-        }
-      ]
-    }
+          content: "@SamchonGithub",
+        },
+      ],
+    };
   },
-}
+};
 
-export default config
+export default config;
