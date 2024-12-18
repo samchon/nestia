3a03a1fd2dc7052bd58ee231ed5798e19e943d13
Jeongho Nam <samchon.github@gmail.com>
2024-12-18 18:07:37 +0900

    Update publish.js

--- deploy/publish.js
+++ deploy/publish.js
@@ -32,13 +32,11 @@ const setup = ({ tag, name, directory, version }) => {
   fs.writeFileSync(file, JSON.stringify(info, null, 2), "utf8");
   if (fs.existsSync(`${directory}/package-lock.json`))
     fs.rmSync(`${directory}/package-lock.json`);
-  try {
-    execute({
-      cwd: directory,
-      script: `npm publish --tag ${tag} --access public${tag === "latest" ? " --provenance" : ""}`,
-      studio: "inherit",
-    });
-  } catch {}
+  execute({
+    cwd: directory,
+    script: `npm publish --tag ${tag} --access public${tag === "latest" ? " --provenance" : ""}`,
+    studio: "inherit",
+  });
 
   // ROLLBACK THE PACKAGE.JSON
   for (const r of rollbacks) r();
