5903fc8761882bcfe2345117c027618777aa187b
Jeongho Nam <samchon.github@gmail.com>
2025-01-07 02:19:43 +0900

    Fix mis-building config

--- packages/agent/src/internal/NestiaChatAgentConstant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NestiaChatAgentConstant = void 0;
-var NestiaChatAgentConstant;
-(function (NestiaChatAgentConstant) {
-    NestiaChatAgentConstant.RETRY = 3;
-    NestiaChatAgentConstant.ELITICISM = true;
-})(NestiaChatAgentConstant || (exports.NestiaChatAgentConstant = NestiaChatAgentConstant = {}));
