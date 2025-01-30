e55ab5413f83546d79a9cd7639fb4f5c7bcff045
Jeongho Nam <samchon.github@gmail.com>
2025-01-30 22:13:10 +0900

    Website playground button as menu

--- website/pages/_meta.js
+++ website/pages/_meta.js
@@ -13,7 +13,24 @@ export default {
   },
   playground: {
     title: "💻 Playground",
-    type: "page",
+    type: "menu",
+    items: {
+      boilerplate: {
+        title: "📦 Boilerplate",
+        href: "https://stackblitz.com/github/samchon/nestia-start?file=README.md&view=editor",
+        newWindow: true,
+      },
+      websocket: {
+        title: "📡 WebSocket",
+        href: "https://stackblitz.com/~/github.com/samchon/tgrid.example.nestjs?file=src/calculate.test.ts&view=editor",
+        newWindow: true,
+      },
+      chat: {
+        title: "📦 A.I. Chatbot",
+        href: "/chat/playground",
+        newWindow: true,
+      },
+    },
   },
   editor: {
     title: "🛠️ Nestia Editor",
