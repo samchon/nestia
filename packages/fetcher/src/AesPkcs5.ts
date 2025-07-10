458c2d0c68915e91ac27ca77d02e5f21562bc1c3
Jeongho Nam <samchon.github@gmail.com>
2025-07-10 15:23:45 +0900

    Remove typia from IConnection

--- packages/fetcher/src/AesPkcs5.ts
+++ packages/fetcher/src/AesPkcs5.ts
@@ -3,10 +3,10 @@ import crypto from "crypto";
 /**
  * Utility class for the AES-128/256 encryption.
  *
- *   - AES-128/256
- *   - CBC mode
- *   - PKCS#5 Padding
- *   - Base64 Encoding
+ * - AES-128/256
+ * - CBC mode
+ * - PKCS#5 Padding
+ * - Base64 Encoding
  *
  * @author Jeongho Nam - https://github.com/samchon
  */
@@ -17,7 +17,7 @@ export namespace AesPkcs5 {
    * @param data Target data
    * @param key Key value of the encryption.
    * @param iv Initializer Vector for the encryption
-   * @return Encrypted data
+   * @returns Encrypted data
    */
   export function encrypt(data: string, key: string, iv: string): string {
     const bytes: number = key.length * 8;
@@ -35,7 +35,7 @@ export namespace AesPkcs5 {
    * @param data Target data
    * @param key Key value of the decryption.
    * @param iv Initializer Vector for the decryption
-   * @return Decrypted data.
+   * @returns Decrypted data.
    */
   export function decrypt(data: string, key: string, iv: string): string {
     const bytes: number = key.length * 8;
@@ -44,7 +44,6 @@ export namespace AesPkcs5 {
       key,
       iv,
     );
-
     return decipher.update(data, "base64", "utf8") + decipher.final("utf8");
   }
 }
