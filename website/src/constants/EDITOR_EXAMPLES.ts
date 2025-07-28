7e4578c87350d29d3a43e8a8ef8421aae87ba21b
Bouzid Badreddine <ubugnu@gmail.com>
2025-07-28 18:44:58 +0100

    fix broken urls

--- website/src/constants/EDITOR_EXAMPLES.ts
+++ website/src/constants/EDITOR_EXAMPLES.ts
@@ -3,37 +3,37 @@ export const EDITOR_EXAMPLES: IAsset[] = [
     title: "BBS (Bullet-in Board System)",
     package: "@samchon/bbs",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v3.1/bbs.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v3.1/bbs.json",
   },
   {
     title: "Shopping Mall",
     package: "@samchon/shopping",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v3.1/shopping.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v3.1/shopping.json",
   },
   {
     title: "Clickhouse",
     package: "clickhouse",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v3.0/clickhouse.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v3.0/clickhouse.json",
   },
   {
     title: "Fireblocks",
     package: "fireblocks",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v3.0/fireblocks.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v3.0/fireblocks.json",
   },
   {
     title: "Uber",
     package: "uber",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v2.0/uber.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v2.0/uber.json",
   },
   {
     title: "OpenAI",
     package: "openai",
     swagger:
-      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/examples/v3.0/openai.json",
+      "https://raw.githubusercontent.com/samchon/openapi/refs/heads/master/test/examples/v3.0/openai.json",
   },
 ];
 interface IAsset {
