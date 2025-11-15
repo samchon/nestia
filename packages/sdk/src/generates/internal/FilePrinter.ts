d0c4b6ccd056c9df7301cbe3866b45e0e96f7c16
Jeongho Nam <samchon.github@gmail.com>
2025-11-16 01:06:27 +0900

    more strict

--- packages/sdk/src/generates/internal/FilePrinter.ts
+++ packages/sdk/src/generates/internal/FilePrinter.ts
@@ -17,7 +17,10 @@ export namespace FilePrinter {
           .split("\r\n")
           .join("\n")
           .split("\n")
-          .map((str) => ` * ${str.split("*/").join("*\\\\/")}`),
+          .map(
+            (str) =>
+              ` * ${str.split("*/").join("*\\\\/").split("*\\/").join("*\\\\/")}`,
+          ),
         "",
       ].join("\n"),
       true,
