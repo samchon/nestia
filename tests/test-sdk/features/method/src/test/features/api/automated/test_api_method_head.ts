1f96c50899d765128cf9c8d146735cfb1959f47b
Jeongho Nam <samchon.github@gmail.com>
2026-03-04 11:10:19 +0900

    ignore sdk generated files

--- tests/test-sdk/features/method/src/test/features/api/automated/test_api_method_head.ts
+++
@@ -1,8 +0,0 @@
-import typia from "typia";
-
-import api from "../../../../api";
-
-export const test_api_method_head = async (connection: api.IConnection) => {
-  const output = await api.functional.method.head(connection);
-  typia.assert(output);
-};
