d15fca9af01be6ef8371f81bceb404ca14a31a58
Jeongho Nam <samchon.github@gmail.com>
2026-03-05 10:42:40 +0900

    complete configuration

--- deploy/next.bash
+++ deploy/next.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-bash README.sh
+bash deploy/README.bash
 
-cd ..
 pnpm bumpp "$1" --no-commit --no-tag --no-push --recursive --yes
 pnpm --filter=./packages/* -r publish --tag next --access public --no-git-checks
