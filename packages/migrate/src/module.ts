42609b6ef9698997a2ba2faeafded835cda53c8f
Jeongho Nam <samchon.github@gmail.com>
2026-03-12 14:48:47 +0900

    re-exports

--- packages/migrate/src/module.ts
+++ packages/migrate/src/module.ts
@@ -1,6 +1,7 @@
 export * from "./NestiaMigrateApplication";
-export * from "./structures/INestiaMigrateContext";
-export * from "./structures/INestiaMigrateConfig";
+export * from "./structures/index";
 
 export * from "./archivers/NestiaMigrateFileArchiver";
 export * from "./executable/NestiaMigrateCommander";
+
+export * from "./programmers/index";
---
+++ packages/migrate/src/structures/index.ts
@@ -0,0 +1,4 @@
+export * from "./INestiaMigrateConfig";
+export * from "./INestiaMigrateContext";
+export * from "./INestiaMigrateController";
+export * from "./INestiaMigrateFile";
